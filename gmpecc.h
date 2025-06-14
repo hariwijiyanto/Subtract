c2d9e7c5bde86d137fe9ac5daac2cebf5306172d
crewchill <50104273+hariwijiyanto@users.noreply.github.com>
2025-06-15 01:36:13 +0700

    Update gmpecc.h

--- gmpecc.h
+++ gmpecc.h
@@ -1,51 +1,17 @@
 #ifndef GMPECC_H
-
 #define GMPECC_H
-
-
 
 #include <gmp.h>
 
+typedef struct {
+    mpz_t x, y;
+    int infinity;
+} gmpecc_point_t;
 
-
-struct Point {
-
-    mpz_t x;
-
-    mpz_t y;
-
-};
-
-
-
-struct Elliptic_Curve {
-
-    mpz_t p;
-
-    mpz_t n;
-
-};
-
-
-
-extern struct Elliptic_Curve EC;
-
-extern struct Point G;
-
-extern struct Point DoublingG[256];
-
-
-
-void Point_Doubling(struct Point *P, struct Point *R);
-
-void Point_Addition(struct Point *P, struct Point *Q, struct Point *R);
-
-void Scalar_Multiplication(struct Point P, struct Point *R, mpz_t m);
-
-void Point_Negation(struct Point *A, struct Point *S);
-
-void init_doublingG(struct Point *P);
-
-
+void gmpecc_point_init(gmpecc_point_t *P);
+void gmpecc_point_clear(gmpecc_point_t *P);
+void gmpecc_point_copy(gmpecc_point_t *R, const gmpecc_point_t *P);
+int gmpecc_point_equal(const gmpecc_point_t *P, const gmpecc_point_t *Q);
+void gmpecc_point_mulsub_g(gmpecc_point_t *R, const gmpecc_point_t *P, const mpz_t k);
 
 #endif
